5a5b889057d94d33537eb2e14914c109993f8e45
Mohammed Yusuf Khan <getmykhan@gmail.com>
2017-08-14 16:43:08 -0700

    new app personal

--- django-tutorials/learndjango/learndjango/urls.py
+++ django-tutorials/learndjango/learndjango/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('webapp.urls')),
+    url(r'^$', include('personal.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls'))
+    url(r'^webapp/', include('webapp.urls')),
+    url(r'^personal/', include('personal.urls'))
 ]
